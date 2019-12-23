3df01442e7c8e2e534e3d78aa320bb2d69b88041
Oscar Castelblanco <oscarjcr95@gmail.com>
2019-12-23 15:05:44 -0500

    Create DomainInfo.go

---
+++ entities/DomainInfo.go
@@ -0,0 +1,22 @@
+package entities
+
+type ServerInformationModel struct {
+  Address string
+  SslGrade string
+  Country string
+  Owner string
+}
+
+type DomainModel struct {
+  Servers []ServerInformationModel
+  ServersChanged bool
+  SslGrade string
+  PreviousSslGrade string
+  Logo string
+  Title string
+  is_down bool
+}
+
+func MakeServerInformationModel( _address string, _sslgrade string, _country string, _owner string ) ( ServerInformationModel ) {
+  return ServerInformationModel{ _address, _sslgrade, _country, _owner}
+}
