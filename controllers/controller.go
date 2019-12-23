289ed210a5da345b714e9c82c9fcf77383ed6031
Oscar Castelblanco <oscarjcr95@gmail.com>
2019-12-23 15:04:14 -0500

    Update controller.go

--- controllers/controller.go
+++ controllers/controller.go
@@ -2,12 +2,13 @@ package controllers
 
 import (
   "net/http" //Http handling
-  //"encoding/json" //Json parsing
+  "encoding/json" //Json parsing
   "github.com/gorilla/mux"
+  "services"
 )
 
 func GetServerInformation ( w http.ResponseWriter, r *http.Request ) {
-	params := mux.Vars(r)
+	params := mux.Vars( r )
 
   if len(params) == 0 {
     http.Error( w, http.StatusText( http.StatusInternalServerError ), http.StatusInternalServerError )
@@ -19,5 +20,18 @@ func GetServerInformation ( w http.ResponseWriter, r *http.Request ) {
 		http.Error( w, http.StatusText( http.StatusInternalServerError ), http.StatusInternalServerError )
 	}
 
-  JsonResponse( w, http.StatusOK, "{'data': 'success'}" )
+  //Model with data to be returned
+  response, error := services.GetServerInformationService( domain )
+
+  if error != nil {
+    http.Error( w, http.StatusText( http.StatusInternalServerError ), http.StatusInternalServerError )
+  }
+
+  data, parseerror := json.Marshal( response );
+
+  if parseerror != nil {
+    http.Error( w, http.StatusText( http.StatusInternalServerError ), http.StatusInternalServerError )
+  }
+
+  JsonResponse( w, http.StatusOK, data);
 }
