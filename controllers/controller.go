6e48e8ca5b29396448a5bdccd9599c1868a079e5
Oscar Castelblanco <oscarjcr95@gmail.com>
2019-12-23 20:44:15 -0500

    Update controller.go

--- controllers/controller.go
+++ controllers/controller.go
@@ -5,33 +5,62 @@ import (
   "encoding/json" //Json parsing
   "github.com/gorilla/mux"
   "services"
+  "cockroachdb"
 )
 
 func GetServerInformation ( w http.ResponseWriter, r *http.Request ) {
 	params := mux.Vars( r )
 
   if len(params) == 0 {
-    http.Error( w, http.StatusText( http.StatusInternalServerError ), http.StatusInternalServerError )
+    http.Error( w, "params header is not present in request", http.StatusInternalServerError )
+    return
   }
 
 	domain := params["server"]
 
 	if domain == "" {
-		http.Error( w, http.StatusText( http.StatusInternalServerError ), http.StatusInternalServerError )
+		http.Error( w, "domain is empty", http.StatusInternalServerError )
 	}
 
   //Model with data to be returned
   response, error := services.GetServerInformationService( domain )
 
   if error != nil {
-    http.Error( w, http.StatusText( http.StatusInternalServerError ), http.StatusInternalServerError )
+    http.Error( w, error.Error(), http.StatusInternalServerError )
   }
 
   data, parseerror := json.Marshal( response );
 
   if parseerror != nil {
-    http.Error( w, http.StatusText( http.StatusInternalServerError ), http.StatusInternalServerError )
+    http.Error( w, parseerror.Error(), http.StatusInternalServerError )
   }
 
   JsonResponse( w, http.StatusOK, data);
+}
+
+func GetHistory ( w http.ResponseWriter, r *http.Request ) {
+  db, dberror := cockroachdb.OpenConnection();
+
+  if dberror != nil {
+    http.Error( w, dberror.Error(), http.StatusInternalServerError )
+    return
+  }
+
+  //Model with data to be returned
+  response, error := services.GetHistory( db )
+
+  if error != nil {
+    http.Error( w, error.Error(), http.StatusInternalServerError )
+    return
+  }
+
+  data, parseerror := json.Marshal( response );
+
+  if parseerror != nil {
+    http.Error( w, parseerror.Error(), http.StatusInternalServerError )
+    return
+  }
+
+  JsonResponse( w, http.StatusOK, data);
+
 }
