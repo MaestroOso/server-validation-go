5fd5e1105af43c6bb5db5cd18feaf44a6df27033
Oscar Castelblanco <oscarjcr95@gmail.com>
2019-12-23 20:44:37 -0500

    Update jsonresponse.go

--- controllers/jsonresponse.go
+++ controllers/jsonresponse.go
@@ -6,5 +6,6 @@ import (
 
 func JsonResponse( w http.ResponseWriter, httpStatus int, jsondata []byte ) {
   w.WriteHeader( httpStatus )
+  w.Header().Set("Content-Type", "application/json")
   w.Write( jsondata )
 }
