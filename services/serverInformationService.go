01ab7c85c04075ce1b7175ad3857b1bdffce7534
Oscar Castelblanco <oscarjcr95@gmail.com>
2019-12-23 20:45:59 -0500

    Update serverInformationService.go

--- services/serverInformationService.go
+++ services/serverInformationService.go
@@ -6,12 +6,14 @@ import (
   "fmt"
   "ssllabs"
   "utils"
+  "cockroachdb"
+  "database/sql"
 )
 
 func GetServerInformationService( domain string ) ( entities.DomainModel, error ) {
     var model entities.DomainModel
 
-
+    //Get Information from SSL labs
     ssllabsresponse, ssllabserror := ssllabs.SslLabs( domain );
 
     if ssllabserror != nil {
@@ -24,6 +26,7 @@ func GetServerInformationService( domain string ) ( entities.DomainModel, error 
 
     for i:=0; i<len( ssllabsresponse.Endpoints ); i++ {
 
+      //Get the WhoIs information for each of the servers
       fmt.Println("Searching for", ssllabsresponse.Endpoints[i].IpAddress)
       whoisresponse, whoiserror := whois.WhoIs( ssllabsresponse.Endpoints[i].IpAddress )
 
@@ -47,6 +50,24 @@ func GetServerInformationService( domain string ) ( entities.DomainModel, error 
 
     fmt.Println("The resulting entity is", model)
 
+    //Get Information from Web Page
+    logo, title, webpageerror := utils.GetWebpageInfo( domain )
+    if webpageerror != nil {
+      return entities.DomainModel{}, webpageerror
+    }
+
+    model.Logo = logo
+    model.Title = title
 
     return model, nil
+}
+
+func GetHistory( db *sql.DB ) ( []cockroachdb.DomainDbModel, error ){
+  domains, err := cockroachdb.GetDomains( db )
+
+  if err != nil {
+    return []cockroachdb.DomainDbModel{}, err
+  }
+
+  return domains, nil
 }
