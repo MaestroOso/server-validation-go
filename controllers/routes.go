0cbea176bb166d5e01470ade5cacf3cdb8ae7049
Oscar Castelblanco <oscarjcr95@gmail.com>
2019-12-23 20:44:58 -0500

    Update routes.go

--- controllers/routes.go
+++ controllers/routes.go
@@ -3,6 +3,7 @@ package controllers
 import (
     "net/http"
     "github.com/gorilla/mux"
+    "fmt"
 )
 
 type Route struct {
@@ -17,13 +18,20 @@ var routes = []Route {
         "GET",
         GetServerInformation,
     },
+    Route{
+        "/history",
+        "GET",
+        GetHistory,
+    },
 }
 
 func InitializeRouter() ( *mux.Router ) {
 
     router := mux.NewRouter()
+    fmt.Println("Created Router")
 
     for i:=0; i<len(routes); i++ {
+      fmt.Println("Adding route", routes[i].Url)
       router.HandleFunc( routes[i].Url, routes[i].HandlerFunc ).Methods( routes[i].Method )
     }
 
