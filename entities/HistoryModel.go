0dce64af53fee73c4dae25d21b79dbd0ad4296d9
Oscar Castelblanco <oscarjcr95@gmail.com>
2019-12-26 17:19:49 -0500

    Update HistoryModel.go

--- entities/HistoryModel.go
+++ entities/HistoryModel.go
@@ -1,9 +1,30 @@
 package entities
 
-type DomainDataModel struct {
+import (
+  "time"
+)
+
+type HistoryServerModel struct {
+  Address string
+  Ssl_grade string
+  Country string
+  Owner string
+}
+
+type HistoryDomainConsultModel struct {
+  Consult_time time.Time
+  Ssl_grade string
+  Title string
+  Logo string
+  Is_down bool
+  Server []HistoryServerModel
+}
+
+type HistoryDomainModel struct {
   Host string
+  Servers []HistoryDomainConsultModel
 }
 
 type HistoryModel struct {
-  Items []DomainDataModel
+  Items []HistoryDomainModel
 }
