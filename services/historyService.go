2be363c3dc7d4b7293d5527453c08b16f57192ce
Oscar Castelblanco <oscarjcr95@gmail.com>
2019-12-26 23:03:01 -0500

    Update historyService.go

--- services/historyService.go
+++ services/historyService.go
@@ -4,11 +4,20 @@ import (
   "entities"
   "log"
   "cockroachdb/repository"
-  "database/sql"
+  "cockroachdb"
 )
 
-func GetHistory( db *sql.DB ) ( entities.HistoryModel, error ){
+func GetHistory( ) ( entities.HistoryModel, error ) {
   log.Println( "Initializing GetHistory" )
+
+  //Db Connection
+  db, dberror := cockroachdb.OpenConnection();
+
+  if dberror != nil {
+    log.Println("Database error:", dberror.Error() )
+    return entities.HistoryModel{}, dberror
+  }
+
   //Get all looked up domains
   domains, err := repository.GetDomains( db )
 
@@ -31,6 +40,18 @@ func GetHistory( db *sql.DB ) ( entities.HistoryModel, error ){
 
     for j := 0; j < len(domainconsult); j++ {
         responseHistoryDomainConsult := entities.HistoryDomainConsultModel{ domainconsult[j].Consult_time, domainconsult[j].Ssl_grade, domainconsult[j].Title, domainconsult[j].Logo, domainconsult[j].Is_down, []entities.HistoryServerModel{} }
+
+        server, serverdberror := repository.SearchServerByDomainConsultId( db, domainconsult[j].Id )
+
+        if serverdberror != nil {
+          return entities.HistoryModel{}, domainconsultdberror
+        }
+
+        for k := 0; k < len(server); k++ {
+          responseServer := entities.HistoryServerModel{ server[k].Address, server[k].Ssl_grade, server[k].Country, server[k].Owner }
+
+          responseHistoryDomainConsult.Server = append( responseHistoryDomainConsult.Server, responseServer )
+        }
 
         responseHistoryDomain.Servers = append( responseHistoryDomain.Servers, responseHistoryDomainConsult );
     }
