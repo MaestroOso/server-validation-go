48db14ee3cffbc3db25b506d93404731cc12f7e0
Oscar Castelblanco <oscarjcr95@gmail.com>
2019-12-26 23:22:35 -0500

    Update jsonresponse.go

--- controllers/jsonresponse.go
+++ controllers/jsonresponse.go
@@ -6,6 +6,7 @@ import (
 
 func JsonResponse( w http.ResponseWriter, httpStatus int, jsondata []byte ) {
   w.Header().Set("Content-Type", "application/json")
+  w.Header().Set("Access-Control-Allow-Origin", "*")
   w.WriteHeader( httpStatus )
   w.Write( jsondata )
 }
