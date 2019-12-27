4e9d4717d20fd256948d0c7f44e1231e168a35b4
Oscar Castelblanco <oscarjcr95@gmail.com>
2019-12-26 23:04:14 -0500

    Update webpageutils.go

--- utils/webpageutils.go
+++ utils/webpageutils.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"io/ioutil"
   "regexp"
+	"strings"
 )
 
 func GetWebpageInfo( domain string ) ( string, string, error ) {
@@ -28,12 +29,14 @@ func GetWebpageInfo( domain string ) ( string, string, error ) {
 
   //Get Icon
   iconvalue := string( GetWebPageData( GetWebPageData( data, `<link[^>]*rel="shortcut icon"[^>]*>` ), `href="[^"]*"` ) )
+	iconvalue = RemoveUnwantedString ( RemoveUnwantedString( iconvalue, `href="` ), `"` );
   if iconvalue != "" {
     icon = iconvalue
   }
 
   //Get Title
-  titlevalue := string( GetWebPageData( GetWebPageData( data, `<title>[^<]*</title>` ), `>.*<` ) )
+	titlevalue := string( GetWebPageData( GetWebPageData( data, `<title>[^<]*</title>` ), `>.*<` ) )
+  titlevalue = RemoveUnwantedString( RemoveUnwantedString( titlevalue, `<` ) , `>` )
   if titlevalue != "" {
     title = titlevalue
   }
@@ -42,6 +45,13 @@ func GetWebpageInfo( domain string ) ( string, string, error ) {
 }
 
 func GetWebPageData( webpage []byte , expression string ) ( []byte ) {
+	if len( webpage ) == 0{
+			return []byte{}
+	}
   compiledRegex := regexp.MustCompile( expression )
   return compiledRegex.Find( webpage )
+}
+
+func RemoveUnwantedString( webpage string, substring string  ) ( string ){
+	return strings.Replace( webpage, substring, "", 1)
 }
